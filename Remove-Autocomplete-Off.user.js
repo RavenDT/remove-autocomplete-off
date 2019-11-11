3e3afeb67f480e077e75d25fe83f75e7b0fe224a
RavenDT <ravendt@gmail.com>
2019-11-10 20:54:17 -0500

    Fixed match/include, which wasn't working

--- Remove-Autocomplete-Off.user.js
+++ Remove-Autocomplete-Off.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Remove Autocomplete=Off
 // @namespace    https://github.com/RavenDT/remove-autocomplete-off
-// @version      0.1
+// @version      0.2
 // @description  A Tampermonkey script to remove autocomplete="off" from form elements so your password manager can autofill.
 // @author       RavenDT
 // @downloadURL  https://raw.githubusercontent.com/RavenDT/remove-autocomplete-off/master/Remove-Autocomplete-Off.user.js
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
-// @match        https?://*
+// @include      /https?://.*/
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
