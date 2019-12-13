48ce145624d60bf27dad090ce444a96d157943d0
RavenDT <ravendt@gmail.com>
2019-12-13 01:06:15 -0500

    Trying to address added form elements from LastPass

--- Remove-Autocomplete-Off.user.js
+++ Remove-Autocomplete-Off.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove Autocomplete=Off
 // @namespace    https://github.com/RavenDT/remove-autocomplete-off
-// @version      0.2
+// @version      0.3
 // @description  A Tampermonkey script to remove autocomplete="off" from form elements so your password manager can autofill.
 // @author       RavenDT
 // @downloadURL  https://raw.githubusercontent.com/RavenDT/remove-autocomplete-off/master/Remove-Autocomplete-Off.user.js
@@ -15,6 +15,25 @@
   "use strict";
   /* global $ */
 
-  $("[autocomplete=off]").removeAttr("autocomplete");
-})();
+  function addNameToFields() {
+    if (!$("input[placeholder='username']").attr("name")) {
+      $("input[placeholder='username']").attr("name","username");
+    }
+    if (!$("input[placeholder='password']").attr("name")) {
+      $("input[placeholder='password']").attr("name","password");
+    }
+  }
 
+  function removeAutocompleteOff() {
+    $("[autocomplete='off']:not([type*='hidden']):not([style*='iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAA'])").removeAttr("autocomplete");
+  }
+
+  function _RemoveAutocompleteOff_bootstrap() {
+    setTimeout(addNameToFields, 750);
+    setTimeout(removeAutocompleteOff, 750);
+  }
+
+  // Start the script
+  _RemoveAutocompleteOff_bootstrap();
+
+})();
